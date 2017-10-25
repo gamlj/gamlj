a91fe23e1ad19673d362937d590d51bb0e02ef10
mcfanda@gmail.com <mcfanda@gmail.com>
2017-10-25 17:48:26 +0200

    version

---
+++ inst/newversion.R
@@ -0,0 +1,15 @@
+
+library(yaml)
+check.version<-function() {
+desc<-yaml.load_file("DESCRIPTION")
+versio<-desc$Version
+j000<-yaml.load_file("jamovi/0000.yaml")
+j000$version
+if (j000$version!=desc$Version) {
+    cat("The version does not correspond in the jamovi e description file")
+    cat(paste("jamovi:",j000$version))
+    cat(paste("DESCRIPTION:",j000$version))
+    return(FALSE)   
+}
+desc$Version
+}
