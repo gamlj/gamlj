d18cceea97a7a920b752016d6b4ea14c51fe0540
mcfanda@gmail.com <mcfanda@gmail.com>
2017-10-27 18:44:08 +0200

    o

--- inst/checks.R
+++ inst/checks.R
@@ -17,6 +17,14 @@ tapply(predict(model),dat$twogroups,mean)
 lsmeans::lsm(model)
 
 dat<-read.csv2("data/dat3x2x2_mixed.csv")
+dat$cluster<-factor(dat$cluster)
+dat$wfac<-factor(dat$wfac)
+dat$bfac<-factor(dat$bfac)
+
+model<-lmer(y~(1|cluster)+wfac*bfac+x,data=dat)
+q<-summary(model)
+
+
 
 lsmeans::lsmeans(model, "twogroups")
 
@@ -28,8 +36,10 @@ dat$dic<-factor(dat$dic)
 
 dat$x<-dat$x-mean(dat$x)+2000
 model<-lm(y~bfac*x,data = dat)
-lsmeans::lsmeans(model, "bfac")
 model<-lm(y~bfac*dic,data = dat)
+
+lsmeans::lsmeans(model, "bfac")
+
 q<-lsmeans::lsmeans(model,c("bfac"))
 x<-print(q)
 
