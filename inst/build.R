929d19559dea3c69efef910fe4aefb5481cba22a
marcello <mcfanda@gmail.com>
2017-12-18 20:00:21 +0100

    clear

--- inst/build.R
+++
@@ -1,8 +0,0 @@
-#Set our working directory. 
-#This helps avoid confusion if our working directory is 
-#not our site because of other projects we were 
-#working on at the time. 
-#setwd("/Users/Nick/personal_site")
-
-#render your sweet site. 
-rmarkdown::render_site(input = "./docssource/")
